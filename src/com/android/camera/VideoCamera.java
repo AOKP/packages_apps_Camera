ef57a948fdfa935db621ee87458eefd4f52eb75b
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-04-26 18:44:47 -0700

    VideoCamera: fix sound playing when volume zoom is enabled
    Change-Id: I415c0b96f445636e954144c66149ddd1a3ea1e8e
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -1095,6 +1095,11 @@ public class VideoCamera extends ActivityBase
                     onShutterButtonClick();
                 }
                 return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (mVolumeZoom) {
+                    return true;
+                }
         }
         return super.onKeyUp(keyCode, event);
     }
