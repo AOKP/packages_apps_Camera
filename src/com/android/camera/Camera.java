f67b36bca4be63001f19122917f4bbd277052b5f
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-07-05 19:52:23 +0200

    Camera: prevent camera from running away with itself when taking multiple pictures.
    The problem arises when mSnapshotOnIdle gets set to true but never gets reset back to false.
    The scenario this would happen would be when attempting to take another picture while a picture is being taken.
    
    Thanks to TheUnkn0wn (XDA).
    
    Change-Id: Ifd1a29a7e8477b6e5bcc48727c52d12cdfc59e3f

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -2089,6 +2089,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         mCameraDevice.setParameters(mParameters);
 
         if (mSnapshotOnIdle) {
+            mSnapshotOnIdle = false;
             mHandler.post(mDoSnapRunnable);
         }
     }
